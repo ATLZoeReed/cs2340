c5ac2a8adfb8be53210bfbe16e1289ae85e5e064
P1ck13 <reegoja@gmail.com>
2025-02-12 20:37:07 -0500

    add search bar to home page and fix footer

--- moviesstore/home/urls.py
+++ moviesstore/home/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home.index'),
     path('about', views.about, name='home.about'),
+    path('search/', views.search, name='home.search'),
 ]
--- moviesstore/home/views.py
+++ moviesstore/home/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
+from movies.models import Movie
+
 
 def index(request):
-    template_data = {}
-    template_data['title'] = 'Movies store'
-    return render(request, 'home/index.html', {
-        'template_data': template_data})
+    template_data = {'title': 'Movies Store'}
+    return render(request, 'home/index.html', {'template_data': template_data})
+
+
+def search(request):
+    query = request.GET.get('q', '')
+    movies = Movie.objects.filter(name__icontains=query) if query else []
+
+    template_data = {'title': 'Search Results'}
+    return render(request, 'home/index.html', {'template_data': template_data, 'movies': movies})
+
 def about(request):
     template_data = {}
     template_data['title'] = 'About'
     return render(request, 'home/about.html',
                   {'template_data': template_data})
+
+
