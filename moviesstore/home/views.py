7b1d3992ed530af4dee76fd18c0d09c8116ecc95
junhong <junhongwang31@gmail.com>
2025-02-16 00:14:04 -0500

    small fixes

--- moviesstore/home/views.py
+++ moviesstore/home/views.py
@@ -9,20 +9,13 @@ def index(request):
         type = "search"
     else:
         movies = Movie.objects.order_by('date')
-        movies = movies[len(movies) - 5:]
+        if len(movies) > 5:
+            movies = movies[len(movies) - 5:]
         movies.reverse()
         type = "recent"
     print(search_term, movies)
     template_data = {'title': 'Movies Store', 'movies': movies, 'search_term': search_term, 'type': type}
     return render(request, 'home/index.html', {'template_data': template_data})
-
-
-# def search(request):
-#     query = request.GET.get('search', '')
-#     movies = Movie.objects.filter(name__icontains=query) if query else None
-#     print('movies', movies)
-#     template_data = {'title': 'Search Results'}
-#     return render(request, 'home/index.html', {'template_data': template_data, 'movies': movies})
 
 def about(request):
     template_data = {}
